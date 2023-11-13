12cc4697ebeb4658a6a092a50d09279ba21130c2
DavideCiammetti <davideciammetti97@gmail.com>
2023-11-13 19:32:16 +0100

    feat: aggiunta commenti e revisione codice

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 'use strict';
-
+// prendo dal dom l'elemento <ul>
 const lista = document.querySelector('.list');
 console.log(lista);
-
+// creo una lista già definita per la spesa 
 const spesa = ['pasta', 'pane', 'latte', 'uova', 'pesce', 'banane', 'peperoni'];
 let i = 0; 
-
+// creo un ciclo while per scorrere la lista fino a che la variabile i non è === alla lunghezza dell'array
 while(i < spesa.length){
+    // stampo in maiuscolo gli elementi della spesa
     console.log(spesa[i].toUpperCase());
+    // creo elementi lista <li>
     const prodotto = document.createElement('li');
-    prodotto.classList.add('prodotto');
+    // stampo elementi spesa
     prodotto.textContent = spesa[i];
+    // aggiungo la classe prodotto a lista 
     lista.append(prodotto);
+    // aggiungo ad i 1
     i++;
 }
